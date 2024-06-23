f3c36518290a1b84b95ea9685f087f94eb4bf048
michaelboegner <cmikeb@gmail.com>
2024-06-23 17:25:28 -0400

    Fixed to parse based on slicing and delimiter type. However, not accounting for multiple delmiters in one text. I think I need recursion for that.

--- src/helpers.py
+++ src/helpers.py
@@ -2,11 +2,17 @@ from textnode import TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for old_node in old_nodes:
-        old_node = old_node.text.split(f"{delimiter}")
-        new_nodes = []
-        for node in old_node:
-            if text_type in node:
-                new_nodes.append(TextNode(node, text_type))
-            else:
-                new_nodes.append(TextNode(node, "text"))
+        first_delimiter = old_node.text.find(delimiter)
+        second_delimiter = old_node.text.find(delimiter, first_delimiter+1)
+        if second_delimiter == None:
+            raise ValueError("Delimiter missing open or closing character.")
+        new_nodes = []      
+        if delimiter == "**":
+            new_nodes.append(TextNode(old_node.text[:first_delimiter], "text"))
+            new_nodes.append(TextNode(old_node.text[first_delimiter+2:second_delimiter], "bold"))
+            new_nodes.append(TextNode(old_node.text[second_delimiter+2:], "text"))
+        if delimiter == "`":
+            new_nodes.append(TextNode(old_node.text[:first_delimiter], "text"))
+            new_nodes.append(TextNode(old_node.text[first_delimiter+1:second_delimiter], "code"))
+            new_nodes.append(TextNode(old_node.text[second_delimiter+1:], "text"))
         return new_nodes
