2717dc86c3965c1aca26a39a7c752d90faa1435e
michaelboegner <cmikeb@gmail.com>
2024-06-25 13:40:10 -0400

    monstrosity of logic to try and parse a string

--- src/helpers.py
+++ src/helpers.py
@@ -1,18 +1,26 @@
 from textnode import TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    new_node = []      
     for old_node in old_nodes:
-        first_delimiter = old_node.text.find(delimiter)
-        second_delimiter = old_node.text.find(delimiter, first_delimiter+1)
-        if second_delimiter == None:
+        delimiter_indices = []
+        for i in range(len(old_node.text)):
+            if delimiter == "**":
+                if old_node.text[i] == "*" and old_node.text[i+1] == "*":
+                    delimiter_indices.append(i + 1)
+            elif old_node.text[i] == delimiter:
+                delimiter_indices.append(i)
+        if len(delimiter_indices) % 2 > 0:
             raise ValueError("Delimiter missing open or closing character.")
-        new_nodes = []      
-        if delimiter == "**":
-            new_nodes.append(TextNode(old_node.text[:first_delimiter], "text"))
-            new_nodes.append(TextNode(old_node.text[first_delimiter+2:second_delimiter], "bold"))
-            new_nodes.append(TextNode(old_node.text[second_delimiter+2:], "text"))
-        if delimiter == "`":
-            new_nodes.append(TextNode(old_node.text[:first_delimiter], "text"))
-            new_nodes.append(TextNode(old_node.text[first_delimiter+1:second_delimiter], "code"))
-            new_nodes.append(TextNode(old_node.text[second_delimiter+1:], "text"))
-        return new_nodes
+        for i in range(len(delimiter_indices)):
+            if i == 0:
+                new_node.append(old_node.text[:delimiter_indices[i]])
+            elif i == len(delimiter_indices)-1:
+                new_node.append(old_node.text[delimiter_indices[i-1]:delimiter_indices[i]])
+                new_node.append(old_node.text[delimiter_indices[i]:])
+            else:
+                new_node.append(old_node.text[delimiter_indices[i-1]:delimiter_indices[i]])
+            print("THIS IS NEW NODES ====", new_node)
+        new_nodes.append(new_node)
+    
