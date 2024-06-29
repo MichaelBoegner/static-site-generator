8f5509449ded3fa3f28f5fcff2254662f00e82fd
michaelboegner <cmikeb@gmail.com>
2024-06-29 17:02:27 -0400

    markdown images parser written with basic unit test

--- src/test_helpers.py
+++ src/test_helpers.py
@@ -1,8 +1,8 @@
 import unittest
-from helpers import split_nodes_delimiter
+from helpers import split_nodes_delimiter, extract_markdown_images
 from textnode import TextNode
 
-class Testhelpers(unittest.TestCase):
+class TestParser(unittest.TestCase):
     def test_code_delimiter(self):
         node = TextNode("This is text with a `code block` word", "text")
         actual = split_nodes_delimiter([node], "`", "code")  
@@ -30,6 +30,11 @@ class Testhelpers(unittest.TestCase):
             TextNode("bolded", "bold"),
             TextNode(" word", "text")
         ]
-        print("THIS IS ACTUAL = ", actual)
         self.assertEqual(expected, actual)
     
+class TestMarkdownImagesParser(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+        expected = [("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"), ("another", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png")]
+        actual = extract_markdown_images(text)
+        self.assertEqual(expected, actual)
