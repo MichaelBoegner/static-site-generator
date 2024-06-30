cb238f54e06c738b77e2fdda1896e3fa1420a8e1
michaelboegner <cmikeb@gmail.com>
2024-06-30 11:40:59 -0400

    refactered replaces to one line and removed prints. Need to rename variables and refactor for ease of readability and efficiency. In short, it's simply working

--- src/helpers.py
+++ src/helpers.py
@@ -27,30 +27,21 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 def extract_markdown_images(text):
     markeddown_images = []
     image_check = re.findall(r"!\[\w+\]", text)
-    print("THIS IS TEXT AFTER IMAGE CHECK: ", text)
     image_text_list = []
     for image_text in image_check:
-        new_text = image_text.replace('!','')
-        new_text = new_text.replace('[','')
-        new_text = new_text.replace(']','')
+        new_text = image_text.replace('!','').replace('[','').replace(']','')
         image_text_list.append(new_text)
-    print("THIS IS IMAGE TEXT: ", image_text_list)
     if len(image_text_list) > 0:
-        print("THIS IS TEXT BEFORE IMAGE_URL: ", text)
         image_url = re.findall(r"(http|ftp|https)\:\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])", text)
         listed_image_url = []
         for url in image_url:
             listed_image_url.append(list(url))
         for url in listed_image_url:
-            url.insert(1, "://")
-        print("THIS IS IMAGE URL: ", listed_image_url)
-        
+            url.insert(1, "://")        
         new_image_url = []
         for url in listed_image_url:
             string = ''.join(url)
             new_image_url.append(string)
-        print("THIS IS NEW IMAGE URL: ", new_image_url)
         for i in range(len(image_url)):
             markeddown_images.append((image_text_list[i], new_image_url[i]))
-        print("THIS IS MARKDOWNIMAGES: ", markeddown_images)
     return markeddown_images
